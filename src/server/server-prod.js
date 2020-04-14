3bc9bfb0298f088b422c7ded1170c2b3cf3a4a00
alejolo311 <alejolo311@gmail.com>
2020-04-13 19:17:14 -0500

    change the static path

--- src/server/server-prod.js
+++ src/server/server-prod.js
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
