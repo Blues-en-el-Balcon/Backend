585201337891e7c879e147cdb67a8b73faff2ce6
alejolo311 <alejolo311@gmail.com>
2020-04-13 21:59:50 -0500

    add example test

--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -7,9 +7,9 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('*', (req, res, next) => {
-    res.send("Hello");
-  });
+app.get('*', (req, res) => {
+  res.send('Hello');
+});
 
 app.listen(PORT, () => {
   console.log(`App listening to ${PORT}....`);
